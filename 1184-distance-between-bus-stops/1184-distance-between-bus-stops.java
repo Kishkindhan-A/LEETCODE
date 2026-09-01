4ab369447ce849de521e35e14d77ea9e091fac29
Kishkindhan-A <kishkindhanawork@gmail.com>
2026-09-01 21:40:35 +0530

    Time: 0 ms (100%), Space: 44.3 MB (41.57%) - LeetHub

---
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+      
+        int totalSum=0;
+        for(int i=0;i<distance.length;i++){
+            totalSum=totalSum+distance[i];
+        }
+        int rightSum=0;
+        int leftSum=0;
+        for(int i=start;i<distance.length;i++){
+
+           if (i==destination){
+                break;
+            } 
+          rightSum=rightSum+distance[i];
+           
+          if(i==distance.length-1){
+                i=-1;
+            }
+             
+        }
+
+    
+        leftSum=totalSum-rightSum;
+
+        if(leftSum<rightSum){
+           return leftSum;
+    }else{
+        return rightSum;
+    }
+    }
+}
